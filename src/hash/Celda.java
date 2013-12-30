ddb7d56d391bf5d68b2521476170ed36f22524ab
jaymdq <bcaimmi@gmail.com>
2013-12-30 18:35:47 -0300

    Commit Brian

--- src/hash/Celda.java
+++ src/hash/Celda.java
@@ -6,7 +6,7 @@ import common.estructura.Elemento;
 
 
 public class Celda implements Serializable{
-
+	// Brian
 	private static final long serialVersionUID = 1L;
 	
 	public static final int VIRGEN = 0;
