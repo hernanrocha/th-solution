3c4960c6f3c4139d7c1fb9ead63206c1d06fe1f0
jaymdq <bcaimmi@gmail.com>
2013-12-30 18:38:56 -0300

    Bueno basta de commits

--- src/hash/Celda.java
+++ src/hash/Celda.java
@@ -6,8 +6,7 @@ import common.estructura.Elemento;
 
 
 public class Celda implements Serializable{
-	// Brian
-	// Hernan
+	
 	private static final long serialVersionUID = 1L;
 	
 	public static final int VIRGEN = 0;
